2d3df60ffe6141628b6417b17a8df0ed4d52417c
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2024-06-10 11:37:41 -0700

    project section added with styling

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,23 @@
 import React from "react";
+import styles from "./ProjectsStyles.module.css"
+import searchEngineLogo from "../../assets/search-engine.png"
+import ProjectCard from "../../common/ProjectCard";
 
-function Proejcts() {
+
+function Projects() {
     return (
-        <div> projects component</div>
+        <section id="projects" className={styles.container}>
+            <h1 className="sectionTitle">Projects</h1>
+            <div className={styles.projectsContainer} >
+                <ProjectCard 
+                src={searchEngineLogo} 
+                link="https://github.com/Moe131/Search_engine_web_app"
+                title="Search Engine Web Application"
+                description = "Search engine web application developed with Python and Flask that allows searching over hundreds of thousand of websites."
+                />
+            </div>
+        </section>
     )
 }
 
-export default Proejcts;
+export default Projects;
