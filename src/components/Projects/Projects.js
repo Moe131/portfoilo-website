a46e3bc4df923a885cb31df597560dcc2209c12f
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2024-06-10 11:57:53 -0700

    projects section completed

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./ProjectsStyles.module.css"
 import searchEngineLogo from "../../assets/search-engine.png"
+import hotelReservationLogo from "../../assets/hotel.png"
+import crawlerLogo from "../../assets/crawler.jpg"
 import ProjectCard from "../../common/ProjectCard";
 
 
@@ -15,6 +17,20 @@ function Projects() {
                 title="Search Engine Web Application"
                 description = "Search engine web application developed with Python and Flask that allows searching over hundreds of thousand of websites."
                 />
+                
+                <ProjectCard 
+                src={hotelReservationLogo} 
+                link="https://github.com/Moe131/Hotel_Reservation_System"
+                title="Hotel Reservation System"
+                description = "Simple hotel reservation system built with Python and Tkinter to store, manage and delete bookings using SQLite database."
+                />
+
+                <ProjectCard 
+                src={crawlerLogo} 
+                link="https://github.com/Moe131/Web-Crawler"
+                title="Web Crawler"
+                description = "A Python-based web crawler designed to efficiently scrape, parse, and store data from websites avoiding loops and duplicate websites "
+                />      
             </div>
         </section>
     )
