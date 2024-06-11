d07678136dafd9d1863f4afb7673709839f636df
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2024-06-10 17:27:27 -0700

    projects and hero styling updated for big screen sizes

--- src/common/ProjectCard.js
+++ src/common/ProjectCard.js
@@ -4,7 +4,7 @@ function ProjectCard(props) {
     return (
         <a href={props.link} target= "_blank">
             <img className="hover" src={props.src} alt={props.title+ " logo"}/>
-            <h3> {props.title}</h3>
+            <h2> {props.title}</h2>
             <p>{props.description} </p>
         </a>
     )
